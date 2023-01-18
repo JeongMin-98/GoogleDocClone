442b672df72564f0b637ec32b8d96167d01e7579
JeongMin-98 <jeongmin981@gmail.com>
2023-01-18 22:59:25 +0900

    [ADD] Google Sign in using Nodejs

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -1,31 +1,60 @@
+import 'dart:convert';
+
+import 'package:google_doc_clone/models/error_model.dart';
+import 'package:google_doc_clone/models/user_model.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart';
+import 'package:google_doc_clone/constants.dart';
 
 final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
     googleSignIn: GoogleSignIn(),
+    client: Client(),
   ),
 );
 
-
+final userProvider = StateProvider<UserModel?>((ref) => null);
 
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
+  final Client _client;
 
-  AuthRepository({
-    required GoogleSignIn googleSignIn,
-  }) : _googleSignIn = googleSignIn;
+  AuthRepository({required GoogleSignIn googleSignIn, required Client client})
+      : _googleSignIn = googleSignIn,
+        _client = client;
 
-  void signInWithGoogle() async {
+  Future<ErrorModel> signInWithGoogle() async {
+    ErrorModel error = ErrorModel(
+      error: 'Some unexpected error occurred.',
+      data: null,
+    );
     try {
       final user = await _googleSignIn.signIn();
       if (user != null) {
-        print(user.email);
-        print(user.displayName);
-        print(user.photoUrl);
+        final userAcc = UserModel(
+            name: user.displayName!,
+            email: user.email,
+            profilePic: user.photoUrl!,
+            uid: '',
+            token: '');
+
+        var res = await _client.post(Uri.parse('$host/api/signup'),
+            body: userAcc.toJson(),
+            headers: {'Contenct-Type': 'application/json; charset=UTF-8'});
+
+        switch (res.statusCode) {
+          case 200:
+            final newUser = userAcc.copyWith(
+              uid: jsonDecode(res.body)['user']['_id'],
+            );
+            error = ErrorModel(error: null, data: newUser);
+            break;
+        }
       }
     } catch (e) {
-      print(e);
+      error = ErrorModel(error: e.toString(), data: null);
     }
+    return error;
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const authRouter = require("./routes/auth")
 const PORT = process.env.PORT | 3001;
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(authRouter);
+
 const DB = require('./config/keys');
-
-
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB.mongoURI, {
