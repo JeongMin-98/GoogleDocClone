53448d458109d530003ea338d2306126e6e3103b
JeongMin-98 <jeongmin981@gmail.com>
2023-01-18 16:25:21 +0900

    [ADD] add mongodb

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const PORT = process.env.PORT | 3001;
 
 const app = express();
+
+const DB = require('./config/keys');
+
+
+
+mongoose.set('strictQuery', true);
+mongoose.connect(DB.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected...")).catch(error => console.log(error))
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`connected at port ${PORT}`);
